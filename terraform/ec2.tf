e9e47debd9cbfa13e40e307901c0e4e856a131f8
Akshansh <akshanshsingh00@gmail.com>
2025-08-27 23:16:12 +0530

    EC2 instance creation using terraform

---
+++ terraform/ec2.tf
@@ -0,0 +1,63 @@
+# Key pair for EC2 instance
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-ec2-key"
+  public_key = file("terraform-ec2-key.pub")
+}
+
+# VPC for EC2 instance
+resource "aws_default_vpc" "default" {
+#   tags = {
+#     Name = "Default VPC"
+#   }
+}
+
+# Security group for EC2 instance
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-ec2-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_default_vpc.default.id # Interpolation
+
+  # inbound rules
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "SSH from anywhere"
+  }
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Access to HTTP from anywhere"
+  }
+
+  # outbound rules
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "terraform_ec2" {
+      ami           = "ami-0360c520857e3138f" # Ubuntu 24
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_tls.name]
+  root_block_device {
+    volume_size = 15
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "terraform-ec2-instance"
+  }
+}
